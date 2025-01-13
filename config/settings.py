2863b69beb2b4fe2178b74a30304573887d54760
Wilfex <wilfex81@gmail.com>
2025-01-13 19:52:18 +0300

    feat(auth): implement user login

--- config/settings.py
+++ config/settings.py
@@ -47,7 +47,9 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     
-    #app
+    'rest_framework_swagger',
+    'drf_yasg' 
+    
     'users'
 ]
 
--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+import requests
 
-# Create your views here.
+
+
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
